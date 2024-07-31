e9faa053e0f145e45bc97a65c4260fef0c5fcecb
Hassan Faisal <iamhassanfaisal@gmail.com>
2024-07-31 13:32:41 +0500

    new image add

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,15 +13,15 @@ const hero = async () => {
     <section className="mx-auto max-w-2xl  px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-32">
-          <h1
+          <div
             className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8
             md:text-6xl"
           >
             Buy trendy and affordable clothes from us!
-          </h1>
+          </div>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            We sell only the most exclusive and high quality products for you.we
-            are the best so come and shop now
+          
+
           </p>
         </div>
         {/* image */}
